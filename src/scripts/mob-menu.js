f3a777330729c9f5eae73f7d3fa96b07bed55bec
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-08-16 00:09:47 +0300

    fix bene cards -minimize circle, refactor js mob-menu

--- src/scripts/mob-menu.js
+++ src/scripts/mob-menu.js
@@ -1,24 +1,23 @@
+const burgerMenu = document.getElementById('burger-menu');
+const mobMenu = document.getElementById('mob-menu');
+const closeBtn = document.getElementById('mob-close');
+const menuLinks = mobMenu.querySelectorAll('a');
+const actionBtn = mobMenu.querySelector('.action');
 
-    const burgerMenu = document.getElementById('burger-menu');
-    const mobMenu = document.getElementById('mob-menu');
-    const closeBtn = document.getElementById('mob-close');
-    const menuLinks = mobMenu.querySelectorAll('a');
+function openMenu() {
+    mobMenu.classList.add('active');
+    document.body.classList.add('no-scroll');
+}
 
-    // Function to open the mobile menu
-    function openMenu() {
-        mobMenu.classList.add('active'); 
-        document.body.classList.add('no-scroll');
-    }
-
-    // Function to close the mobile menu
-    function closeMenu() {
-        mobMenu.classList.remove('active');
+function closeMenu() {
+    mobMenu.classList.remove('active');
     document.body.classList.remove('no-scroll');
-    }
+}
 
-    // Event listeners
-    burgerMenu.addEventListener('click', openMenu);
-    closeBtn.addEventListener('click', closeMenu);
-    menuLinks.forEach(link => {
-        link.addEventListener('click', closeMenu);
-    });
+burgerMenu.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+});
+
+actionBtn.addEventListener('click', closeMenu);
